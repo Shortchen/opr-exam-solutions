889fc2cda46c29e6d6705ea2fa2b9d4cc4e9b0ed
Long Hoang <gialong.hoang@protonmail.com>
2025-06-13 17:06:51 +0200

    Add ss19/aufgabe2_1

---
+++ ss19/aufgabe2_1/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct A {
+    void f() {
+        cout << "A::f\n";
+        g();
+        h();
+    }
+
+    virtual void g() { cout << "A::g\n"; }
+    void h() { cout << "A::h\n"; }
+};
+
+struct B : A {
+    void g() { cout << "B::g\n"; }
+    virtual void h() { cout << "B::h\n"; }
+};
+
+struct C : B {
+    virtual void g() { cout << "C::g\n"; }
+    virtual void h() const { cout << "C::h\n"; }
+};
+
+int main() {
+    B b;
+    C c;
+    B &z = c;
+    b.f();
+    z.g();
+    z.h();
+    c.f();
+}
+
+
